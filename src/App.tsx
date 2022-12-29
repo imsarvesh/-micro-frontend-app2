5bb9f49a35dcd941297067be8b6486bfb5ba1e70
SArvesh Kesharwani <me@imsarvesh.com>
2022-12-29 20:56:26 +0000

    module fedration

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { Button } from "Components/Button";
+
+
+const App = () => (
+    <div className="m-3">
+        <Suspense fallback="Loading..">
+        <Button className="!bg-blue-600" data-testid="helloFriendsButton">Hello Friends 🚀</Button>
+        </Suspense>
     </div>
-  );
-}
+);
 
 export default App;
