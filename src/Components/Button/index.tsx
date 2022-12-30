f8e31dae73dc388717b7c48609a379a34d9acde5
SArvesh Kesharwani <me@imsarvesh.com>
2022-12-30 14:53:36 +0000

    first commit

--- src/Components/Button/index.tsx
+++ src/Components/Button/index.tsx
@@ -1,15 +1,24 @@
-import classnames from "classnames";
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 
 interface Props {
-    children?: ReactNode;
-    className?: string;
-    "data-testid"?: string;
+  children?: ReactNode;
+  className?: string;
+  "data-testid"?: string;
 }
 
-const Button = ({ children, className, "data-testid": dataTestId }: Props) => (
-    <button type="button" data-testid={dataTestId} className={classnames("p-4 bg-green-600 rounded-lg font-bold text-white mt-5 hover:bg-gray-600", className)}>
-        Hello Doston
-    </button>);
+const Button = ({
+  children,
+  className,
+  "data-testid": dataTestId,
+  ...props
+}: Props) => (
+  <button
+    type="button"
+    data-testid={dataTestId}
+    {...props}
+  >
+    ✔{children}
+  </button>
+);
 
 export { Button };
